6f3eb84b1bbdbfa15af809d765291e1cc8ea0267
Andrey Butenko <whitespysoftware@gmail.com>
2019-04-27 16:43:06 +0300

    Changed supported Python and Django versions

--- metatags/tests/test_settings.py
+++ metatags/tests/test_settings.py
@@ -2,12 +2,12 @@ SECRET_KEY = 'test_key'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3'
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+    },
 }
 
 INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'metatags'
+    'metatags',
 ]
--- setup.py
+++ setup.py
@@ -1,28 +1,35 @@
-import os
 from setuptools import setup, find_packages
-
-
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-    README = readme.read()
 
 
 setup(
     name='django-simple-metatags',
     version='0.9.1',
-    description='''
-    The django application, that allows attach title, keywords and description meta tags for
-    site's pages.
+    description='''The django application, that allows attach title, keywords 
+    and description meta tags for site's pages.
     ''',
     author='Andrey Butenko',
     author_email='whitespysoftware@yandex.ru',
     url='https://github.com/whitespy/django-simple-metatags',
-    long_description=README,
+    long_description=open('README.rst').read(),
     packages=find_packages(),
     include_package_data=True,
     platforms='any',
-    install_requires=['django>=1.8'],
+    install_requires=['django>=1.11.20'],
     classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5'
-    ]
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
 )
