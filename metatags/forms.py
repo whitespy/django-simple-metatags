2af0429827353d84d48a4aee69bfe0e9b68419e5
Andrey Butenko <whitespysoftware@gmail.com>
2019-04-28 13:05:12 +0300

    Refactored admin forms

--- metatags/forms.py
+++ metatags/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.conf import settings
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext, ugettext_lazy as _
 
 from .models import MetaTag
 
@@ -14,17 +14,28 @@ class InlineMetaTagForm(forms.ModelForm):
     def media(self):
         _media = forms.Media(css={'all': ('css/meta_tags.css',)})
         if 'modeltranslation' in settings.INSTALLED_APPS:
-            _media.add_css({'all': ('modeltranslation/css/tabbed_translation_fields.css',)})
-            _media.add_js(('modeltranslation/js/force_jquery.js', 'js/jquery-ui.min.js',
-                           'modeltranslation/js/tabbed_translation_fields.js'))
+            _media.add_css({
+                'all': ('modeltranslation/css/tabbed_translation_fields.css',),
+            })
+            _media.add_js((
+                'modeltranslation/js/force_jquery.js', 'js/jquery-ui.min.js',
+                'modeltranslation/js/tabbed_translation_fields.js',
+            ))
         return _media
 
 
 class MetaTagForm(InlineMetaTagForm):
-    url = forms.RegexField(label=_('URL-path'), max_length=100, regex=r'^[-\w/\.~]+$',
-                           help_text=_("Example: '/about/contact/'. Make sure to have leading and trailing slashes."),
-                           error_messages={'invalid': _("This value must contain only letters, numbers, dots, "
-                                                        "underscores, dashes, slashes or tildes.")})
+    url = forms.RegexField(
+        label=_('URL-path'),
+        max_length=100,
+        regex=r'^[-\w/\.~]+$',
+        help_text=_("Example: '/about/contact/'. Make sure to have leading and trailing slashes."),
+        error_messages={
+            'invalid': _(
+                "This value must contain only letters, numbers, dots, underscores, dashes, slashes or tildes."
+            ),
+        },
+    )
 
     class Meta:
         fields = ('url', 'title', 'keywords', 'description')
@@ -33,14 +44,13 @@ class MetaTagForm(InlineMetaTagForm):
         url = self.cleaned_data['url']
 
         if not url.startswith('/'):
-            raise forms.ValidationError(_('URL is missing a leading slash.'))
+            raise forms.ValidationError(gettext('URL is missing a leading slash.'))
 
         if (not url.endswith('/') and settings.APPEND_SLASH and
-                'django.middleware.common.CommonMiddleware' in settings.MIDDLEWARE_CLASSES):
-            raise forms.ValidationError(_('URL is missing a trailing slash.'))
+                'django.middleware.common.CommonMiddleware' in settings.MIDDLEWARE):
+            raise forms.ValidationError(gettext('URL is missing a trailing slash.'))
 
-        if MetaTag.objects.filter(url=url).exists():
-            if self.instance is None or url != self.instance.url:
-                raise forms.ValidationError(_('Meta tags for a given URL-path have already been identified.'))
+        if MetaTag.objects.filter(url=url).exists() and (self.instance is None or url != self.instance.url):
+            raise forms.ValidationError(gettext('Meta tags for a given URL-path have already been identified.'))
 
         return url
