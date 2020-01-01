9898738ca1a88a65b2d3defb8953755e897d475c
Andrey Butenko <whitespysoftware@gmail.com>
2020-01-01 15:19:37 +0200

    Bumped version to 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-simple-metatags',
-    version='0.9.2',
+    version='1.0.0',
     description="The django application, that allows attach title, keywords "
                 "and description meta tags for site's pages.",
     author='Andrey Butenko',
@@ -17,16 +17,16 @@ setup(
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
-        'Framework :: Django',
-        'Framework :: Django :: 2.0',
-        'Framework :: Django :: 2.1',
-        'Framework :: Django :: 2.2',
-        'Framework :: Django :: 3.0',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Framework :: Django',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
     ],
 )
