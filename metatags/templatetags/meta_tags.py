21c10bb17fc4f5deb581f09151dee491828d41ba
Andrey Butenko <whitespysoftware@gmail.com>
2020-04-04 15:37:51 +0300

    Removed top level deprecation warning from meta_tags template tags module

--- metatags/templatetags/meta_tags.py
+++ metatags/templatetags/meta_tags.py
@@ -4,19 +4,15 @@ from django.template import Library
 
 from .metatags import include_metatags
 
-warnings.warn(
-    'The meta_tags template module is deprecated and will be removed '
-    'in 3.0.0 in favor of metatags module.',
-    DeprecationWarning,
-)
 register = Library()
 
 
 @register.inclusion_tag('metatags/includes/metatags.html', takes_context=True)
 def include_meta_tags(context, model_instance=None, default_title='', default_keywords='', default_description=''):
     warnings.warn(
-        'The include_meta_tags template tag is deprecated and will be removed '
-        'in 3.0.0 in favor of include_metatags template tag.',
+        'The include_meta_tags template tag is deprecated in favor of '
+        'include_metatags template tag and will be removed in 3.0.0 version.',
         DeprecationWarning,
+        stacklevel=2,
     )
     return include_metatags(context, model_instance, default_title, default_keywords, default_description)
